3866e8533a9099c07ba522c3567ada3f60c07615
Mitchell Quinn <mitchell.quinn@domanistudios.com>
2016-10-21 01:52:47 -0400

    Added new_note form

--- resources/views/new_note.blade.php
+++ resources/views/new_note.blade.php
@@ -2,19 +2,33 @@
 
 @section('content')
 <home :user="user" inline-template>
-    <div class="container">
-        <!-- Application Dashboard -->
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
-                    <div class="panel-body">
-                        Your application's dashboard.safsafsad
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">New Note</div>
+					<div class="panel-body">
+						<form>
+							<div class="form-group">
+								<label for="title">Title:</label>
+								<input type="input" class="form-control" id="title" name="title">
+							</div>
+							<div class="form-group">
+								<label for="body">Body:</label>
+								<textarea class="form-control" id="body" name="body"></textarea>
+							</div>
+							<div class="form-group">
+								<label for="tags">Tags:</label>
+								<select id="tags[]" name="tags[]" multiple="true">
+									<option>Test</option>
+								</select>
+							</div>
+							<button type="submit" class="btn btn-default">Save</button>
+							</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </home>
 @endsection
